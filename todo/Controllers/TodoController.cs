1d5427bbf0c86d159d648daace0216890eaed610
Henrique Assreuy <henrique_assreuy@outlook.com>
2023-01-03 21:29:33 -0300

    First commit with project structure.

---
+++ todo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put() { }
+
+        [HttpDelete]
+        public void Delete() { }
+    }
+}
